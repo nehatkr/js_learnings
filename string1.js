e45d124fb5d3f1cf4a5f89e104ecb8b65bf3848c
nehatkr <110078405+nehatkr@users.noreply.github.com>
2023-10-10 13:25:12 +0000

    day4

---
+++ string1.js
@@ -0,0 +1,14 @@
+ let name = "neha"
+console.log(name[0])
+console.log(name[1])
+
+let friend = 'parkash'
+console.log(friend)
+// Template literals
+let boy1 = "rahul"
+let boy2 = "swate"
+
+// rahul is a friend of shweta
+let sentence = `${boy2} is a friend of ${boy1}`
+console.log(sentence)
+
---
+++ string2.js
@@ -0,0 +1,18 @@
+let name = "Neha"
+// console.log(name.length)
+// console.log(name.toUpperCase())
+// console.log(name.toLowerCase())
+// console.log(name.slice(1,3))
+// console.log(name.replace("Neh" , "Har"))
+
+// let friend = "Naman"
+// console.log(name.concat("is a friend of ",friend , " Ok "))
+// let friend2 = "      Meena      "
+// console.log(friend2.trim())
+let fr = "Shivika" + "Shiv"
+console.log(fr[0])
+console.log(fr[1])
+console.log(fr[2])
+console.log(fr[3])
+console.log(fr)
+// string cant be change , only new string will be return
